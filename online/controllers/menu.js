3c5a1409a9c6fd9f9192d34fecaf0001c4addc03
Abhijith C S <Abhijith.cs@ad.infosys.com>
2016-12-22 00:22:04 +0530

    Cart Validations are working

--- online/controllers/menu.js
+++ online/controllers/menu.js
@@ -27,7 +27,50 @@ angular.module('FullMenu', [])
 
     }); 
 
-    
+    $scope.addToCart = function(code, name, price, variety){
+
+      
+            var info = JSON.parse(localStorage.getItem("itemsInfo")); //getting items from localStorage
+            var i = 0;
+            var flag = -1;
+
+            console.log(info.length) ;
+            while(i<info.length)
+            {
+                //checks if item aldready in cart and returns the position of that object if exists
+                if(info[i].itemCode==code)
+                {
+                    flag = i;
+                    break;
+                }
+                i++; 
+            }   
+            if(flag != -1){
+                var item = JSON.parse(localStorage.itemsInfo);
+                //var info = JSON.parse(localStorage.getItem("itemsInfo"))[x];
+                item[flag].itemQuantity +=1;
+                localStorage.setItem("itemsInfo", JSON.stringify(item));
+                //console.log(info.itemQuantity);
+                console.log((JSON.parse(localStorage.getItem("itemsInfo")))[0]);
+            }
+            else if(flag == -1){
+                var oldItems = JSON.parse(localStorage.getItem('itemsInfo'));
+                var newItem = {"itemCode":code,"itemName":name,"itemQuantity": 1,"itemPrice":price,"itemVariety":variety};
+                oldItems.push(newItem);
+                var x = JSON.parse(localStorage.getItem("itemsInfo")) ;
+                //console.log(x.itemQuantity);
+                localStorage.setItem('itemsInfo', JSON.stringify(oldItems));
+                console.log((JSON.parse(localStorage.getItem("itemsInfo")))[3]);
+                
+            }
+               
+                //var l = JSON.parse(localStorage.getItem("itemsInfo"))
+                //localStorage.setItem('itemsInfo', JSON.stringify(items));
+                //var j =0;
+                //console.log(l);
+                
+                renderCart();
+    }
  
   })
 
