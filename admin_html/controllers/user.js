aa2d8b02c8917132beeb96091c07cc9b64d625f1
anasjafry <anasrazak@gmail.com>
2017-01-15 14:04:49 +0530

    User Profile Page Completed

---
+++ admin_html/controllers/user.js
@@ -0,0 +1,30 @@
+angular.module('UsersApp', [])
+
+  .controller('userOrdersController', function($scope, $http) {
+  	console.log("This works!!");
+  	$http.get("http://localhost/vega-web-app/online/orderhistory.php?id=0&mobile=9043960876").then(function(response) {
+        $scope.user_orders = response.data;       
+    }); 
+    $scope.prevflag=false;
+    $scope.limiter=0;
+    $scope.nextflag=true;
+    $scope.showNext = function(){
+    	$scope.prevflag=true;
+    	$scope.limiter+=5;
+    	$http.get("http://localhost/vega-web-app/online/orderhistory.php?mobile=9043960876&id="+$scope.limiter).then(function(response) {
+        	$scope.user_orders = response.data;
+        	if($scope.user_orders.length < 5){
+        		$scope.nextflag=false;
+        	}       
+    	}); 
+    }
+    $scope.showPrev = function(){
+    	$scope.limiter-=5;
+    	$http.get("http://localhost/vega-web-app/online/orderhistory.php?mobile=9043960876&id="+$scope.limiter).then(function(response) {
+        	$scope.user_orders = response.data;       
+    	});
+    	if($scope.limiter==0){
+    		$scope.prevflag=false;
+    	} 
+    }
+  })
