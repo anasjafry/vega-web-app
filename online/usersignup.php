7f45dfac51449b609ed0e3cf4e5881564bee979c
anasjafry <anasrazak@gmail.com>
2017-02-07 19:58:42 +0530

    User SignUP API

---
+++ online/usersignup.php
@@ -0,0 +1,52 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+
+define('INCLUDE_CHECK', true);
+require 'connect.php';
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$mobile = $_POST['mobile'];
+
+//$token = mysql_real_escape_string($_POST['token']);
+
+$output = [];
+
+$query = "SELECT * from z_users WHERE mobile='{$mobile}'";
+$main = mysql_query($query);
+$rows = mysql_fetch_assoc($main);
+$output = [];
+$status = '';
+$error = '';
+$otp = rand(1000,9999);
+
+if(empty($rows)){
+	$response[] = array(
+		"userid" => $mobile,
+		"isOTPSent" => true
+	);
+	$status = 'success';
+}
+else{
+	$response[] = array(
+		"userid" => $mobile,
+		"isOTPSent" => false
+	);
+	$status = 'fail';
+	$error = 'User aldready exists';
+}
+
+$output[] = array(
+	"response" => $response,
+	"status" => $status,
+	"error" => $error
+);
+
+//$list = array('status' => $flag);    
+echo json_encode($output);
+		
+?>
+
