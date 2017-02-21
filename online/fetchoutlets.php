a568e76bce4d72c0d3f2488570edd31188aca357
Abhijith C S <abhijithcs1993@gmail.com>
2017-02-21 21:10:29 +0530

    Modification to display single outlets info

--- online/fetchoutlets.php
+++ online/fetchoutlets.php
@@ -7,19 +7,43 @@ header('Access-Control-Allow-Credentials: true');
 define('INCLUDE_CHECK', true);
 require 'connect.php';
 
-$_POST = json_decode(file_get_contents('php://input'), true);
+error_reporting(0);
 
+$status = false;
+$error = '';
+
+if(isset($_GET['outletcode'])){
+	$query1 = "SELECT * from z_outlets WHERE code='{$_GET['outletcode']}'";
+	$main1 = mysql_query($query1);
+
+	if($rows1 = mysql_fetch_assoc($main1)){
+	$status = true;
+	$response=array(
+		"name"=> $rows1['name'],
+		"code"=> $rows1['code'],
+		"line1"=> $rows1['line1'],
+		"line2"=> $rows1['line2'],
+		"mobile"=> $rows1['contact'],
+		"lat"=> $rows1['latitude'],
+		"lng"=> $rows1['longitude'],
+		"pictures" => json_decode($rows1['fotos'])
+	);
+	}
+	else{
+		$status = false;
+		$error = "Invalid Code";
+	}
+
+}
+
+else{
 $query = "SELECT DISTINCT city from z_outlets";
 $main = mysql_query($query);
-
-$status = 'fail';
-$error = '';
 
 while($rows = mysql_fetch_assoc($main)){
 	$query1 = "SELECT * from z_outlets WHERE city='{$rows['city']}'";
 	$main1 = mysql_query($query1);
 	$outlets = [];
-
 
 	while($rows1 = mysql_fetch_assoc($main1)){
 		$outlets[]=array(
@@ -32,24 +56,23 @@ while($rows = mysql_fetch_assoc($main)){
 			"lng"=> $rows1['longitude'],
 			"pictures" => json_decode($rows1['fotos'])
 		);
-		$status = 'success';
+		$status = true;
 	}
 
 	$response[]= array(
 		"city" => $rows['city'],
 		"outlets" => $outlets
 	);
+}
 
 }
 
 
+//Final Results
 $output = array(
 	"response" => $response,
 	"status" => $status,
 	"error" => $error
 	);
-//$list = array('status' => $flag);
 echo json_encode($output);
-
-
 ?>
