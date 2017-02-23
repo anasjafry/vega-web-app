2d36cf9178a0d870669c0abee9b6046a85064f6c
Abhijith C S <abhijithcs1993@gmail.com>
2017-02-23 23:36:46 +0530

    Validations added

--- online/newreservation.php
+++ online/newreservation.php
@@ -5,40 +5,62 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Credentials: true');
 error_reporting(0);
 
+//Database Connection
 define('INCLUDE_CHECK', true);
 require 'connect.php';
 
+//Encryption Credentials
+define('SECURE_CHECK', true);
+require 'secure.php';
+
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$userID = $_POST['userID'];
+//Encryption Validation
+if(!isset($_POST['token'])){
+	$output = array(
+		"status" => false,
+		"error" => "Access Token is missing"
+	);
+	die(json_encode($output));
+}
+
+if(!isset($_POST['details'])){
+	$output = array(
+		"status" => false,
+		"error" => "Reservation Object is missing"
+	);
+	die(json_encode($output));
+}
+
+$token = $_POST['token'];
+$decryptedtoken = openssl_decrypt($token, $encryptionMethod, $secretHash);
+$tokenid = json_decode($decryptedtoken, true);
+
+//Expiry Validation
+date_default_timezone_set('Asia/Calcutta');
+$dateStamp = date_create($tokenid['date']);
+$today = date_create(date("Y-m-j"));
+$interval = date_diff($dateStamp, $today);
+$interval = $interval->format('%a');
+
+if($interval > $tokenExpiryDays){
+	$output = array(
+		"status" => false,
+		"error" => "Expired Token"
+	);
+	die(json_encode($output));
+}
+
+
+$userID = $tokenid['mobile'];
 
 //$stub = '{"isDefault": false, "name": "Abhijith C S", "flatNo": "306", "flatName": "Alakananda Hostel", "landmark": "IIT Madras Campus", "area": "IIT Madras", "city": "Chennai", "contact": 9003824036 }';
 $details = $_POST['details'];
 
-$encryptionMethod = "AES-256-CBC";
-$secretHash = "7a6169746f6f6e746f6b656e";
-$token = $_POST['token'];
-$decryptedtoken = openssl_decrypt($token, $encryptionMethod, $secretHash);
-//echo($decryptedtoken);
-$tokenid = json_decode($decryptedtoken,true);
-//$error =
-//$output = [];
-
-date_default_timezone_set('Asia/Calcutta');
-$date = date("j F, Y");
-$time = date("g:i a");
-
-$status = 'fail';
-$error = 'Access Denied. Not Authorized!';
-
-//echo($tokenid['mobile']);
-if($tokenid['mobile'] == $userID){
-	$status = 'success';
+	$status = true;
 	$error = "";
-	$query = "INSERT INTO z_reservations (`userID`, `outlet`, `date` , `time` , `count`) VALUES ('{$userID}','{$details['outlet']}','{$date}','{$time}','{$details['count']}')";
+	$query = "INSERT INTO z_reservations (`userID`, `outlet`, `date` , `time` , `count`) VALUES ('{$userID}','{$details['outlet']}','{$details['date']}','{$details['time']}','{$details['count']}')";
 	$main = mysql_query($query);
-}
-
 
 $output = array(
 	"status" => $status,
@@ -46,5 +68,4 @@ $output = array(
 );
 
 echo json_encode($output);
-
 ?>
