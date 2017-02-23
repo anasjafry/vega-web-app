25ffc5766b6af63fcdb6a681f0339112145d8fce
Abhijith C S <abhijithcs1993@gmail.com>
2017-02-23 23:03:20 +0530

    Minor validations added

--- online/edituser.php
+++ online/edituser.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Credentials: true');
@@ -8,34 +8,61 @@ error_reporting(0);
 define('INCLUDE_CHECK', true);
 require 'connect.php';
 
+//Encryption Credentials
+define('SECURE_CHECK', true);
+require 'secure.php';
+
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$userID = $_POST['userID'];
-$info = $_POST['info'];
-$encryptionMethod = "AES-256-CBC";  
-$secretHash = "7a6169746f6f6e746f6b656e";
+//Encryption Validation
+if(!isset($_POST['token'])){
+	$output = array(
+		"status" => false,
+		"error" => "Access Token is missing"
+	);
+	die(json_encode($output));
+}
+
 $token = $_POST['token'];
 $decryptedtoken = openssl_decrypt($token, $encryptionMethod, $secretHash);
-//echo($decryptedtoken);
-$tokenid = json_decode($decryptedtoken,true);
+$tokenid = json_decode($decryptedtoken, true);
 
-$status = 'fail';
-$error = 'No such user exists!';
-//$error = 
-//$output = [];
-$i = 0;
+//Expiry Validation
+date_default_timezone_set('Asia/Calcutta');
+$dateStamp = date_create($tokenid['date']);
+$today = date_create(date("Y-m-j"));
+$interval = date_diff($dateStamp, $today);
+$interval = $interval->format('%a');
 
-if($tokenid['mobile'] == $userID){
-	$query = "SELECT * from z_users WHERE mobile='{$userID}'";
+if($interval > $tokenExpiryDays){
+	$output = array(
+		"status" => false,
+		"error" => "Expired Token"
+	);
+	die(json_encode($output));
+}
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+
+$status = false;
+$error = 'User does not exist';
+
+
+	$query = "SELECT * from z_users WHERE mobile='{$tokenid['mobile']}'";
 	$main = mysql_query($query);
 	$rows = mysql_fetch_assoc($main);
-	
-	$status = 'success';
+
+	if($rows['mobile']){
+	$status = true;
 	$error = '';
 
-	$query = "UPDATE z_users SET `name`='{$info['name']}' , `email`='{$info['email']}' WHERE mobile='{$userID}'";
+	$query = "UPDATE z_users SET `name`='{$name}' , `email`='{$email}' WHERE mobile='{$rows['mobile']}'";
 	$main = mysql_query($query);
-}
+	}
+
 
 
 $output = array(
@@ -44,6 +71,4 @@ $output = array(
 );
 
 echo json_encode($output);
-		
 ?>
-
